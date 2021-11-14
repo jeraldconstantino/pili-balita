d4c59a4b20943ff274392c7aa0f7b32e6481b88c
Jerald Constantino <jeraldcons.27@gmail.com>
2021-11-14 21:18:59 +0800

    nilagyan ng deskripsyon ang bawat algoritmo

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from kivymd.uix.dialog import MDDialog
 from tungkolSaAplikasyon import tungkol
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
+from deskripsyonAlgoritmo import deskripsyon
 from kivy.network.urlrequest import UrlRequest
 from kivymd.uix.bottomsheet import MDGridBottomSheet
 from kivymd.uix.button import MDFlatButton, MDRaisedButton
@@ -22,6 +23,10 @@ class Resulta(FloatLayout):
 
 
 class BoxLayoutScreen(BoxLayout):
+    pass
+
+
+class DeskripsyonAlgoritmo(BoxLayout):
     pass
 
 
@@ -89,7 +94,8 @@ class Katawan(BoxLayout):
             "TUNGKOL": "information-variant",
             "GABAY": "help",
             "ISARA": "exit-to-app",
-            "PATALASTAS!": "bullhorn-outline",
+            "DESKRIPSYON NG MGA ALGORITMO": "book-information-variant",
+            "PATALASTAS!": "bullhorn-outline"   
         }
 
         for aytem in datos.items():
@@ -111,13 +117,25 @@ class Katawan(BoxLayout):
         elif args[0] == "PATALASTAS!":
             self.ipakita_ang_patalastas()
 
+        elif args[0] == "DESKRIPSYON NG MGA ALGORITMO":
+            self.ipakita_ang_deskripsyon_ng_mga_algoritmo()
+
         else:
             Window.close()
+
+    def ipakita_ang_deskripsyon_ng_mga_algoritmo(self):
+        self.dayalogo_ng_deskripsyon_ng_mga_algoritmo = MDDialog(
+            type="custom",
+            title="DESKRIPSYON NG MGA ALGORITMO",
+            content_cls=DeskripsyonAlgoritmo(),
+            radius=[10],
+        )
+        self.dayalogo_ng_deskripsyon_ng_mga_algoritmo.open()
 
     def ipakita_ang_tungkol_sa_aplikasyon(self):
         self.dayalogo_ng_tungkol_sa_aplikasyon = MDDialog(
             title="PILI-BALITA: DETEKTOR NG MALING BALITA",
-            text= tungkol(),
+            text=tungkol(),
             radius=[10],
         )
         self.dayalogo_ng_tungkol_sa_aplikasyon.open()
@@ -127,10 +145,10 @@ class Katawan(BoxLayout):
             type="custom",
             title="GABAY SA PAGGAMIT NG PILI-BALITA",
             text="1. Humanap ng mga balitang kumakalat sa sosyal midya. Kapag nagdadalawang-isip ka sa balitang iyong nabasa. Maaari mong kopyahin ang kabuuang balita.\n"
-            + "2. Matapos kopyahin ang balita, ilagay ito sa TEXT FIELD. Maaari mo ring pindutin ang PINDUTAN NG PASTE para mailagay ng awtomatiko ang iyong kinopyang balita.\n"
-            + "3. Pindutin ang  PINDUTAN NG TUKLASIN! para maproseso ng aplikasyon ang iyong nailagay na balita.\n"
-            + "4. Pagkatapos iproseso ang datos, ipapakita ng aplikasyon ang porsyento ng kawastuhan ng balita, pati na rin ang husga nito ukol dito.\n"
-            + "5. Maaari mo ring pindutin ang PINDUTAN NG KABUUANG RESULTA upang makita ang kabuuang detalye ng resulta.\n",
+            + "\n2. Matapos kopyahin ang balita, ilagay ito sa TEXT FIELD. Maaari mo ring pindutin ang PINDUTAN NG PASTE para mailagay ng awtomatiko ang iyong kinopyang balita.\n"
+            + "\n3. Pindutin ang  PINDUTAN NG TUKLASIN! para maproseso ng aplikasyon ang iyong nailagay na balita.\n"
+            + "\n4. Pagkatapos iproseso ang datos, ipapakita ng aplikasyon ang porsyento ng kawastuhan ng balita, pati na rin ang husga nito ukol dito.\n"
+            + "\n5. Maaari mo ring pindutin ang PINDUTAN NG KABUUANG RESULTA upang makita ang kabuuang detalye ng resulta.\n",
             radius=[10],
         )
         self.dayalogo_ng_gabay_sa_aplikasyon.open()
